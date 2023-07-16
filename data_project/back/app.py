b992479f2d6f5da854dc3b310c2af7eaada644cf
cjdrudco <tmdals1001@gmail.com>
2023-07-16 12:16:30 +0900

    :memo:update

--- data_project/back/app.py
+++ data_project/back/app.py
@@ -12,15 +12,29 @@ df = pd.read_csv("C:\\Users\\user\\Documents\\dev\\KKC_Python_Project\\data_proj
 
 @app.route('/')
 def main():
+    global df
+    print(df['시간'])
     l = {
-        'x' : df['시간'],
-        'y' : df['득점']
+        'x' : df['시간'].values.tolist(),
+        'y' : df['득점'].values.tolist()
     }
     res = json.dumps(l)
     return res
 
-def prediction():
-    pass
+@app.route('/gain_chart')
+def gain_chart():
+    global df
+
+    teamName = '맨시티'
+    name = df[df['홈'] == teamName]
+    teamScore = name['득점']
+    time = name['시간']
+    l = {
+        'x' : time.values.tolist(),
+        'y' : teamScore.values.tolist(),
+    }
+    res = json.dumps(l)
+    return res
 
 if __name__ == '__main__':
     app.run()
