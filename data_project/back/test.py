c01abe9f76a1b66d8eee662a4c5031a59e143e4b
cjdrudco <tmdals1001@gmail.com>
2023-08-27 12:19:11 +0900

    :memo:update

--- data_project/back/test.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from matplotlib import pyplot as plt
-
-df = pd.read_csv("C:\\Users\\user\\Documents\\dev\\KKC_Python_Project\\data_project\\back\\data.csv", encoding='cp949')
-x = df['시간']
-y = df['득점']
-plt.plot(x, y)
-plt.show()
